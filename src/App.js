4ac4870ce30e4b4791d595015c046dc6333c2353
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-03-14 13:02:29 +0300

    Get authors from the API and delete static authors data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
 
-import authors from "./data.js";
-
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+import Axios from "axios";
 
 class App extends Component {
   state = {
+    authors: [],
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: [],
+    load: true
   };
 
   selectAuthor = author => this.setState({ currentAuthor: author });
 
   unselectAuthor = () =>
-    this.setState({ currentAuthor: null, filteredAuthors: authors });
+    this.setState({ currentAuthor: null, filteredAuthors: this.state.authors });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -42,17 +43,45 @@ class App extends Component {
     }
   };
 
+  fitchAuthorsAPI = async () => {
+    try {
+      const res = await Axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      this.setState({ authors: res.data, filteredAuthors: res.data });
+    } catch (error) {
+      console.error();
+    }
+    this.setState({ load: false });
+  };
+
+  componentDidMount() {
+    this.fitchAuthorsAPI();
+  }
+
   render() {
-    return (
-      <div id="app" className="container-fluid">
-        <div className="row">
-          <div className="col-2">
-            <Sidebar unselectAuthor={this.unselectAuthor} />
+    if (this.state.load) {
+      return (
+        <div className="container-fluid">
+          <div className="row my-4 justify-content-md-center">
+            <div className="col-2">
+              <h1>Loading ..</h1>
+            </div>
           </div>
-          <div className="content col-10">{this.getContentView()}</div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div id="app" className="container-fluid">
+          <div className="row">
+            <div className="col-2">
+              <Sidebar unselectAuthor={this.unselectAuthor} />
+            </div>
+            <div className="content col-10">{this.getContentView()}</div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
